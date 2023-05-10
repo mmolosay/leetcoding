b8844beec7873e2d6c1b2796cdd9c47953ed2dbf
mmolosay <mmolosays@gmail.com>
2023-05-10 11:40:21 +0200

    Solved

---
+++ src/main/kotlin/medium/M59SpiralMatrixII.kt
@@ -0,0 +1,59 @@
+package medium
+
+/**
+ * [59. Spiral Matrix II](https://leetcode.com/problems/spiral-matrix-ii/)
+ */
+fun m59makeSpiralMatrix(order: Int): Array<IntArray> {
+    val result = Array(order) { IntArray(order) }
+    val total = order * order
+    var ordinal = 1
+    var r = 0
+    var c = 0
+    var leftToRight = true
+    var topToBottom = true
+    var fillingHorizontally = true
+    while (ordinal <= total) {
+        if (fillingHorizontally) {
+            if (leftToRight) {
+                if (c + 1 <= order && result[r][c] == 0) {
+                    result[r][c++] = ordinal++
+                } else {
+                    fillingHorizontally = false
+                    leftToRight = false
+                    r++
+                    c--
+                }
+            } else {
+                if (c >= 0 && result[r][c] == 0) {
+                    result[r][c--] = ordinal++
+                } else {
+                    fillingHorizontally = false
+                    leftToRight = true
+                    r--
+                    c++
+                }
+            }
+        } else {
+            if (topToBottom) {
+                if (r + 1 <= order && result[r][c] == 0) {
+                    result[r++][c] = ordinal++
+                } else {
+                    fillingHorizontally = true
+                    topToBottom = false
+                    c--
+                    r--
+                }
+            } else {
+                if (r - 1 >= 0 && result[r][c] == 0) {
+                    result[r--][c] = ordinal++
+                } else {
+                    fillingHorizontally = true
+                    topToBottom = true
+                    c++
+                    r++
+                }
+            }
+        }
+    }
+    return result
+}
