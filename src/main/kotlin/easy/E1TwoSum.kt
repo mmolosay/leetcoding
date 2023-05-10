f5fd04f6b24dfdbf7c4062fc9cc894e9270ea9d8
mmolosay <mmolosays@gmail.com>
2023-05-10 12:31:35 +0200

    Solve

---
+++ src/main/kotlin/easy/E1TwoSum.kt
@@ -0,0 +1,18 @@
+package easy
+
+/**
+ * [1. Two Sum](https://leetcode.com/problems/two-sum/)
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val traversed = mutableMapOf<Int, Int>()
+    for (i in nums.indices) {
+        val counterpart = target - nums[i]
+        val index = traversed[counterpart]
+        if (index != null) {
+            return intArrayOf(i, index)
+        } else {
+            traversed[nums[i]] = i
+        }
+    }
+    error("Unreachable")
+}
