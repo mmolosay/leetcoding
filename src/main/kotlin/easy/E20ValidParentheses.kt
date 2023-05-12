e6dd5c949029682508ee462b9b17a6b9c1afbd0b
mmolosay <mmolosays@gmail.com>
2023-05-12 13:27:05 +0200

    Solve

---
+++ src/main/kotlin/easy/E20ValidParentheses.kt
@@ -0,0 +1,25 @@
+package easy
+
+fun e20_isValid(s: String): Boolean {
+    val opened = ArrayDeque<Char>(s.length / 2)
+    for (char in s) {
+        if (char.isOpeningParentheses) {
+            opened.addLast(char)
+        } else {
+            val lastOpened = if (opened.isEmpty()) return false else opened.removeLast()
+            if (!lastOpened.isParenthesesCounterpartTo(char)) return false
+        }
+    }
+    return opened.isEmpty()
+}
+
+private val Char.isOpeningParentheses: Boolean
+    get() = (this == '(' || this == '{' || this == '[')
+
+private fun Char.isParenthesesCounterpartTo(other: Char): Boolean =
+    when (this) {
+        '(' -> other == ')'
+        '{' -> other == '}'
+        '[' -> other == ']'
+        else -> error("Illegal char")
+    }
