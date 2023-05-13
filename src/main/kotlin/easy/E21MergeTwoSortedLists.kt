0b671e912d55fd850891258d60a62fd37e20bc12
mmolosay <mmolosays@gmail.com>
2023-05-13 16:49:43 +0200

    Solve linearly and recursively

---
+++ src/main/kotlin/easy/E21MergeTwoSortedLists.kt
@@ -0,0 +1,81 @@
+package easy
+
+/**
+ * [21. Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)
+ */
+fun e21_mergeTwoLists_1(list1: ListNode?, list2: ListNode?): ListNode? {
+    if (list1 == null) return list2
+    if (list2 == null) return list1
+    var n1 = list1
+    var n2 = list2
+    val head = if (n1.value <= n2.value) {
+        n1.also { n1 = it.next }
+    } else {
+        n2.also { n2 = it.next }
+    }
+    var tail = head
+    while (n1 != null && n2 != null) {
+        if (n1!!.value <= n2!!.value) {
+            tail.next = n1
+            tail = n1!!
+            n1 = n1?.next
+        } else {
+            tail.next = n2
+            tail = n2!!
+            n2 = n2?.next
+        }
+    }
+    if (n1 == null) {
+        tail.next = n2
+    } else { // n2 == null
+        tail.next = n1
+    }
+    return head
+}
+
+fun e21_mergeTwoLists_2(list1: ListNode?, list2: ListNode?): ListNode? {
+    if (list1 == null) return list2
+    if (list2 == null) return list1
+
+    return if (list1.value > list2.value) {
+        ListNode(list2.value).apply {
+            next = e21_mergeTwoLists_2(list1, list2.next)
+        }
+    } else {
+        ListNode(list1.value).apply {
+            next = e21_mergeTwoLists_2(list1.next, list2)
+        }
+    }
+}
+
+val ListNode.value: Int
+    get() = `val`
+
+// ================== //
+
+fun List<Int>.toListNode(): ListNode? =
+    if (isEmpty()) null
+    else {
+        var head = ListNode(last())
+        var i = lastIndex - 1
+        while (i >= 0) {
+            head = ListNode(get(i)).also { it.next = head }
+            i--
+        }
+        head
+    }
+
+fun ListNode.toList(): List<Int> {
+    var tail = this
+    return buildList {
+        add(tail.value)
+        while (tail.next != null) {
+            add(tail.next!!.value)
+            tail = tail.next!!
+        }
+    }
+}
+
+class ListNode(val `val`: Int) {
+    var next: ListNode? = null
+}
