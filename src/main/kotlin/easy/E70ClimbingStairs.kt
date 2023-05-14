88be0e2e95612a1ab795d00de9aea83a8e21572c
mmolosay <mmolosays@gmail.com>
2023-05-14 15:50:52 +0200

    Refine comment

--- src/main/kotlin/easy/E70ClimbingStairs.kt
+++ src/main/kotlin/easy/E70ClimbingStairs.kt
@@ -9,8 +9,8 @@ fun e70_climbStairs(n: Int): Int {
     var curr = 1 // number of ways to climb to (i - 1) step
     /*
      * For each step i, the number of distinct ways to climb to that step is equal to the sum of the number of
-     * distinct ways to climb to the previous two steps, because we can either take one step from the i-1th step
-     * or two steps from the i-2th step. This recurrence relation is similar to the Fibonacci sequence.
+     * distinct ways to climb to the previous two steps f(n - 1) + f(n - 2), because we can either take one step from
+     * the i-1th step or two steps from the i-2th step. This recurrence relation is similar to the Fibonacci sequence.
      */
     for (i in 1 until n) {
         curr += prev.also { prev = curr }
